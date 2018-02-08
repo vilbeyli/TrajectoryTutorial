35f11f5314bf766ddf0897c3c3f83be204d3e7f6
Volkan Ilbeyli <volkanilbeyli@gmail.com>
2018-02-07 20:36:40 -0800

    functional part-i implementation

--- Assets/Trajectory.cs
+++ Assets/Trajectory.cs
@@ -4,78 +4,30 @@ using UnityEngine;
 
 public class Trajectory : MonoBehaviour
 {
+    // launch variables
     [SerializeField] private Transform TargetObject;
-
-    [Range(1.0f, 6.0f)] public float TargetDistance;
+    [Range(1.0f, 6.0f)] public float TargetRadius;
     [Range(20.0f, 70.0f)] public float LaunchAngle;
 
+    // state
     private bool bTargetReady;
-    private Rigidbody rigid;
     private bool bTouchingGround;
 
-    Vector3 initialPosition;
-    Quaternion initialRotation;
+    // cache
+    private Rigidbody rigid;
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    
+    //-----------------------------------------------------------------------------------------------
 
     // Use this for initialization
     void Start()
     {   
-        bTargetReady = true;
+        bTargetReady = false;
         rigid = GetComponent<Rigidbody>();
         bTouchingGround = true;
         initialPosition = transform.position;
         initialRotation = transform.rotation;
-    }
-
-    // launches the object towards the TargetObject with a given LaunchAngle
-    void Launch()
-    {
-        // shorthands for the formula
-        float R = Vector3.Distance(transform.position, TargetObject.position);
-        float G = -Physics.gravity.y;
-        float alpha = Mathf.Deg2Rad * LaunchAngle;  // in radians
-
-        // rotate the object to face the target
-        transform.LookAt(TargetObject.position);
-
-        // calculate initial speed required to land the cube on target using the formula (9)
-        float Vi = Mathf.Sqrt(R * G / Mathf.Sin(2 * alpha));    // initial speed
-        float Vy = Vi * Mathf.Sin(alpha); // velocity component in upward  direction of local space
-        float Vz = Vi * Mathf.Cos(alpha); // velocity component in forward direction of local space
-
-        // create the velocity vector in local space and get it in global space
-        Vector3 localVelocity = new Vector3(0f, Vy, Vz);
-        Vector3 globalVelocity = transform.TransformDirection(localVelocity);
-
-        // launch the object by setting its initial velocity and flipping its state
-        rigid.velocity = globalVelocity;
-        bTargetReady = false;
-    }
-
-    // Sets a random target around the object based on the TargetDistance
-    void SetNewTarget()
-    {
-        Transform targetTF = TargetObject.GetComponent<Transform>(); // shorthand
-
-        // Think of randomly acquiring a point TargetDistance away from us as the following:
-        // - we start with a vector in the XZ-Plane (ground), let's pick right and scale it w/ TargetDistance.
-        //   (or pick left, forward, back, or any perpendicular vector to the rotation axis, which is up)
-        // - we also add an offset which makes the starting position at the same height level as the target
-        Vector3 startingPoint = Vector3.right * TargetDistance + new Vector3(0, targetTF.position.y, 0);
-
-        //  - we get a random angle to rotate this vector around a circle
-        float randomAngle = Random.Range(0.0f, 360.0f);
-
-        //  - create our rotation quaternion, which will transform our starting point vector [unclear]
-        Vector3 rotationAxis = Vector3.up;  // as our object is on XZ-Plane, we'll use up vector as the rotation axis.
-
-        //  - we'll rotate the starting position vector with the rotation quaternion to get the final 
-        //    random position around the circle of this object.
-        Vector3 randomPoint = Quaternion.AngleAxis(randomAngle, rotationAxis) * startingPoint;
-
-        //  - finally, we'll set the target object's position. 
-        TargetObject.SetPositionAndRotation(randomPoint, targetTF.rotation);
-        
-        bTargetReady = true;
     }
 
 	// Update is called once per frame
@@ -95,6 +47,7 @@ public class Trajectory : MonoBehaviour
 
         if (!bTouchingGround)
         {
+            // update the rotation of the projectile during trajectory motion
             transform.rotation = Quaternion.LookRotation(rigid.velocity) * initialRotation;
         }
 	}
@@ -107,5 +60,53 @@ public class Trajectory : MonoBehaviour
     void OnCollisionExit()
     {
         bTouchingGround = false;
+    }
+
+    // launches the object towards the TargetObject with a given LaunchAngle
+    void Launch()
+    {
+        // rotate the object to face the target
+        transform.LookAt(TargetObject.position);
+
+        // shorthands for the formula
+        float R = Vector3.Distance(transform.position, TargetObject.position);
+        float G = Physics.gravity.y;
+        float alpha = LaunchAngle * Mathf.Deg2Rad;  // in radians
+
+        // calculate initial speed required to land the projectile on target object using the formula (9)
+        float Vi = Mathf.Sqrt(-R * G / Mathf.Sin(2 * alpha));    // initial speed Vo
+        float Vy = Vi * Mathf.Sin(alpha); // velocity component in upward  direction of local space
+        float Vz = Vi * Mathf.Cos(alpha); // velocity component in forward direction of local space
+
+        // create the velocity vector in local space and get it in global space
+        Vector3 localVelocity = new Vector3(0f, Vy, Vz);
+        Vector3 globalVelocity = transform.TransformDirection(localVelocity);
+
+        // launch the object by setting its initial velocity and flipping its state
+        rigid.velocity = globalVelocity;
+        bTargetReady = false;
+    }
+
+    // Sets a random target around the object based on the TargetRadius
+    void SetNewTarget()
+    {
+        Transform targetTF = TargetObject.GetComponent<Transform>(); // shorthand
+        
+        // To acquire our new target from a point around the projectile object:
+        // - we start with a vector in the XZ-Plane (ground), let's pick right (1, 0, 0).
+        //   (or pick left, forward, back, or any perpendicular vector to the rotation axis, which is up)
+        // - We'll use a quaternion to rotate our vector. To create a rotation quaternion, we'll be using
+        //   the AngleAxis() function, which takes a rotation angle and a rotation amount in degrees as parameters.
+        Vector3 rotationAxis = Vector3.up;  // as our object is on the XZ-Plane, we'll use up vector as the rotation axis.
+        float randomAngle = Random.Range(0.0f, 360.0f);
+        Vector3 randomVector = Quaternion.AngleAxis(randomAngle, rotationAxis) * Vector3.right;
+
+        // - scale the randomVector with the target distance
+        // - we also add an offset which makes the starting position at the same height level as the target
+        Vector3 randomPoint = randomVector * TargetRadius + new Vector3(0, targetTF.position.y, 0);
+
+        //  - finally, we'll set the target object's position and update our state. 
+        TargetObject.SetPositionAndRotation(randomPoint, targetTF.rotation);
+        bTargetReady = true;
     }
 }
